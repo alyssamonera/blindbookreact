7b5bd967427508cd61710e1d7b28ddcdb5ba90b3
Alyssa Monera <alyssa.monera@fenomdigital.com>
2026-02-06 11:35:28 -0500

    censors book descriptions

--- components/books-grid.tsx
+++ components/books-grid.tsx
@@ -6,9 +6,9 @@ type BooksGridProps = {
     books: Book[]
 }
 
-export default function BooksGrid({books}) {
-    return <ul>
-        {books.map(book => <li key={book.id} className="py-3">
+export default function BooksGrid({books}: BooksGridProps) {
+    return <ul className="flex flex-wrap">
+        {books.map((book) => <li key={book.id} className="py-3 m-2 whitespace-break-spaces border-amber-600 border-3">
             {book.description}
         </li>)}
     </ul>
--- lib/books.tsx
+++ lib/books.tsx
@@ -1,13 +1,34 @@
 import { franc } from "franc-min";
-const limit = 10;
+import { limit, alphabet } from "@/config";
 
-function getDescription(data: {
+type apiSearchResults = {
+	key: string;
+	authors: string[];
+}[];
+
+type apiBookData = {
+	title: string;
+	authors: string[];
 	description: string | { value: string } | void;
-}) {
+};
+
+type modelSearchResults = {
+	id: string;
+	authors: string[];
+	description: string;
+	title: string;
+};
+
+/**
+ * Simplifies description down to a string and filters for English
+ * @param data - The book data that came back from the API
+ * @returns Either an empty string or the description string value
+ */
+function getDescription(data: apiBookData) {
 	let description = data.description;
 
 	if (!description) {
-		return null;
+		return "";
 	}
 
 	if (typeof description === "object") {
@@ -18,48 +39,111 @@ function getDescription(data: {
 		return description;
 	}
 
-	return null;
+	return "";
 }
 
-function getRandomRange(keys: string[]) {
-	const max = keys.length - limit;
+/**
+ * Picks out a random range of results from the API, within the configured limit
+ * @param apiSearchResults - All of the books that came back from the API
+ * @returns A random integer for the index value
+ */
+function getRandomRange(apiSearchResults: apiSearchResults) {
+	const max = apiSearchResults.length - limit;
 	const min = 0;
 
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-async function batchFetchBooks(keys: string[]) {
-	const results = [];
-    const startPoint = getRandomRange(keys);
+/**
+ * Replaces all instances of the title and author from the description string
+ * @param books The selected array of simplified book objects 
+ * @returns The selected array of simplified book objects, now with censored descriptions
+ */
+function censorBooks(books: modelSearchResults[]) {
+	for (let i = 0; i < books.length; i++) {
+		const currentBook = books[i];
 
-	for (let i = startPoint; i < keys.length; i+= limit) {
-		const batch = keys.slice(i, i + limit);
-		const arrayOfResponses = await Promise.all(
-			batch.map((key) =>
-				fetch(`https://openlibrary.org/${key}.json`, {
+		books[i].description = currentBook.description.replaceAll(
+			currentBook.title,
+			"TITLE",
+		);
+
+		currentBook.authors.forEach((author: string, index) => {
+			const assignedLetter =
+				currentBook.authors.length > 1 ? " " + alphabet[index] : "";
+			const authorRegex = new RegExp(`\\b${author}\\b`, "gi");
+
+			const lastName = author.trim().split(/\s+/).pop() ?? "";
+			const lastNameRegex = new RegExp(`\\b${lastName}\\b`, "gi");
+
+			const authorString = "AUTHOR" + assignedLetter;
+			const lastNameString = "LAST NAME" + assignedLetter;
+
+			books[i].description = books[i].description
+				.replaceAll(authorRegex, authorString)
+				.replaceAll(lastNameRegex, lastNameString);
+		});
+	}
+
+	return books;
+}
+
+/**
+ * Collects more detailed book data via the keys taken from the API
+ * @param apiSearchResults All the book data from the API
+ * @returns An array of simplified book data that only includes what we need
+ */
+async function batchFetchBooks(apiSearchResults: apiSearchResults) {
+	let results: {
+		id: string;
+		title: string;
+		authors: string[];
+		description: string;
+	}[] = [];
+	const allResponses = [];
+	const startPoint = getRandomRange(apiSearchResults);
+
+	for (let i = startPoint; i < apiSearchResults.length; i += limit) {
+		const batch = apiSearchResults.slice(i, i + limit);
+		const currentBatchOfResponses = await Promise.all(
+			batch.map((searchResult) =>
+				fetch(`https://openlibrary.org/${searchResult.key}.json`, {
 					headers: {
 						"User-Agent": "BlindBookDatingApp/1.0 (alyssamonera@gmail.com)",
 					},
-				}).then((r) => r.json())
-                .then((data) => ({id: key, description: getDescription(data)}))
+				})
+					.then((r) => r.json())
+					.then((data: apiBookData) => ({
+						id: searchResult.key,
+						title: data.title,
+						authors: [...searchResult.authors],
+						description: getDescription(data),
+					})),
 			),
 		);
 
-        results.push(...arrayOfResponses.filter(book => !!book.description));
+		allResponses.push(
+			...currentBatchOfResponses.filter((book) => book.description !== ""),
+		);
 
-        if (results.length >= 10) {
-            break;
-        }
+		if (allResponses.length >= 10) {
+			results = allResponses.slice(0, 10);
+			break;
+		}
 
-        await new Promise(r => setTimeout(r, 300));
+		await new Promise((r) => setTimeout(r, 300));
 	}
 
 	return results;
 }
 
+/**
+ * Gets the book descriptions based on user-selected keywords
+ * @returns An array of books with censored descriptions
+ */
 export async function getBooks() {
 	const response = await fetch(
-		"https://openlibrary.org/search.json?subject=romance&limit=500&language=eng",
+		"https://openlibrary.org/search.json?subject=love+fantasy&limit=500&language=eng&fields=key,title,author_name",
 		{
 			headers: {
 				"User-Agent": "BlindBookDatingApp/1.0 (alyssamonera@gmail.com)",
@@ -72,9 +156,15 @@ export async function getBooks() {
 		throw new Error("Failed to fetch books.");
 	}
 
-	const keys = resData.docs.map((doc: { key: string }) => doc.key);
+	const apiSearchResults = resData.docs.map(
+		(doc: { key: string; author_name: string[] }) => ({
+			key: doc.key,
+			authors: doc.author_name,
+		}),
+	);
 
-	const books = await batchFetchBooks(keys);
+	const books = await batchFetchBooks(apiSearchResults);
+	const censoredBooks = censorBooks(books);
 
-	return books;
+	return censoredBooks;
 }
