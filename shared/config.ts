22b7f4f573336b73474c9bed1c465d1c3de463b1
alyssamonera <ahlisa.dm@gmail.com>
2026-04-23 12:55:35 -0400

    feature/fuzzy-search uses token match + fuzzy search to censor description

--- shared/config.ts
+++ shared/config.ts
@@ -52,93 +52,13 @@ export const DUMMY_BOOKS: bookResult[] = [
 	{
 		id: "1",
 		volumeInfo: {
-			description: "This is the skin of a monster Bella",
-			authors: ["Stephenie Meyer"],
-			title: "Twilight",
+			description: "byshe In the summer of 1816, Mary Wollstonecraft Godwin, then eighteen years old, began to write the novel Frankenstein after she and her lover Percy Bysshe Shelley took part in a ghost-story competition at Lord Byron's villa by Lake Geneva. Over the next nine months -- a period which saw their return to England in autumn 1816 and subsequent marriage -- she (with Percy) drafted the entire novel in a form materially different from the two standard editions of 1818 and 1831 which were based on a later fair copy. Until now, no one has been able to read what Mary Shelley herself initially wrote in this original draft of the novel. Going back to the unique draft manuscript of the text held in the Bodleian Library, Charles E. Robinson has teased out Percy Shelley's amendments, isolating them from the story in Mary Shelley's hand. Both texts - with and without Percy's interventions - are presented in this edition, allowing us for the first time to read the story in Mary's original hand and also to see how Percy edited his wife's prose. The results are fascinating. We read a more rapidly paced novel that is arranged in different chapters. Above all, we hear Mary's genuine voice which sounds to us more modern, more immediately colloquial than her husband's learned, more polished style. To this day, Frankenstein remains the most popular work of science fiction. This edition promises to redefine the ways we read the story and perceive the act of its creation.",
+			authors: [
+				'Mary Wollstonecraft Shelley',
+				'Percy Bysshe Shelley'
+			],
+			title: "Frankenstein, Or, The Modern Prometheus",
 		},
 		censoredDescription: "This is the skin of a monster Bella",
-	},
-	{
-		id: "2",
-		volumeInfo: {
-			description: "Hey loca where you been",
-			authors: ["Stephenie Meyer"],
-			title: "New Moon",
-		},
-		censoredDescription: "Hey loca where you been",
-	},
-	{
-		id: "3",
-		volumeInfo: {
-			description: "The angel from that one cancelled show is in this one",
-			authors: ["Stephenie Meyer"],
-			title: "Breaking Dawn",
-		},
-		censoredDescription: "The angel from that one cancelled show is in this one",
-	},
-	{
-		id: "4",
-		volumeInfo: {
-			description:
-				"Paladin tries and fails to kill a goddess and somehow they end up together",
-			authors: ["Author"],
-			title: "Daughter of the Sun",
-		},
-		censoredDescription:
-			"Paladin tries and fails to kill a goddess and somehow they end up together",
-	},
-	{
-		id: "5",
-		volumeInfo: {
-			description: "Your hair is like starlight!",
-			authors: ["Diana Wynne Jones"],
-			title: "Howl's Moving Castle",
-		},
-		censoredDescription: "Your hair is like starlight!",
-	},
-	{
-		id: "6",
-		volumeInfo: {
-			description: "A beauty and the beast metaphor",
-			authors: ["Sarah J. Maas"],
-			title: "A Court of Fey and Flowers",
-		},
-		censoredDescription: "A beauty and the beast metaphor",
-	},
-	{
-		id: "7",
-		volumeInfo: {
-			description: "A lot of parkour and a love triangle",
-			authors: ["I Forget", "A second one"],
-			title: "Steeplejack",
-		},
-		censoredDescription: "A lot of parkour and a love triangle",
-	},
-	{
-		id: "8",
-		volumeInfo: {
-			description: "I vant to suck your blad",
-			authors: ["Bram Stoker"],
-			title: "Dracula",
-		},
-		censoredDescription: "I vant to suck your blad",
-	},
-	{
-		id: "9",
-		volumeInfo: {
-			description: "A guy creates another guy all by himself",
-			authors: ["Mary Shelley"],
-			title: "Frankenstein",
-		},
-		censoredDescription: "A guy creates another guy all by himself",
-	},
-	{
-		id: "10",
-		volumeInfo: {
-			description: "Mr. Darcy fumbles the bag multiple times until he doesn't",
-			authors: ["Jane Austen"],
-			title: "Pride & Prejudice",
-		},
-		censoredDescription: "Mr. Darcy fumbles the bag multiple times until he doesn't",
-	},
+	}
 ];
