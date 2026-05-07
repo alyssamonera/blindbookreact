78cfec2dc16d044dfdcde50eb407df34b5d94a40
alyssamonera <ahlisa.dm@gmail.com>
2026-05-07 14:55:16 -0400

    feature/styling - uses dropdown + adds heading

--- components/homepage/genre-list.tsx
+++ components/homepage/genre-list.tsx
@@ -1,21 +1,36 @@
-import Link from "next/link";
+"use client";
+
 import { genres } from "@/shared/config";
 import { genreInput } from "@/shared/types";
+import { redirect } from "next/navigation";
 
 export default function GenreList() {
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		const genre = formData.get("genre");
+		if (!genre) return;
+
+		redirect(`/books/${genre}`);
+	}
+
 	return (
-		<ul>
-			{Object.keys(genres).map((key) => {
-				const genre = genres[key as genreInput];
-				return (
-					<li key={genre.searchValue} className="hover:underline">
-						<Link href={`/books/${key}`}>{genre.displayValue}</Link>
-					</li>
-				);
-			})}
-			<li key="fairytale" className="hover:underline">
-				<Link href="/books/search?q=fairytale">Fairytale</Link>
-			</li>
-		</ul>
+		<form onSubmit={handleSubmit} className="flex gap-2 items-center">
+			<select required className="bg-gray-300 p-3 rounded" name="genre" defaultValue="">
+				<option value="" disabled>Pick a genre</option>
+				{Object.keys(genres).map((key) => {
+					const genre = genres[key as genreInput];
+					return (
+						<option key={genre.searchValue} value={key}>
+							{genre.displayValue}
+						</option>
+					);
+				})}
+				<option key="fairytale" value="fairytale">
+					Fairytale
+				</option>
+			</select>
+			<button className="block bg-green-800 hover:bg-green-400 text-white hover:text-black cursor-pointer px-3 py-2 rounded">Go</button>
+		</form>
 	);
 }
